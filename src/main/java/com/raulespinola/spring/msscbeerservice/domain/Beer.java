a0a6d1bd2065a09a19a2ee6191bb5be664ba481e
Raul <raulespinola@gmail.com>
2020-01-17 15:21:51 -0300

    Create JPA Entities Beer

---
+++ src/main/java/com/raulespinola/spring/msscbeerservice/domain/Beer.java
@@ -0,0 +1,46 @@
+package com.raulespinola.spring.msscbeerservice.domain;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibertnate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+    private String beerName;
+    private String beerStyle;
+
+    @Column(unique = true)
+    private Long upc;
+
+    private BigDecimal prive;
+    private Integer minOnhand;
+    private Integer quantityToBrew;
+}
